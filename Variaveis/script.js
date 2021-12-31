eb8a87b82bc1161d359bd151364246bd2d3d74d7
Henrique Boiteux <henriqueboiteux62@gmail.com>
2021-12-31 08:23:43 -0300

    VIACEP

--- Variaveis/script.js
+++ Variaveis/script.js
@@ -6,3 +6,15 @@ comida = 'Lasanha'
 console.log(comida)
 
 var pais, professor, time, endereco, rua
+
+function nomeUpCase(nome) {
+    return nome.toUpperCase()
+}
+
+console.log(nomeUpCase('henrique'))
+
+fetch('viacep.com.br/ws/01001000/json/')
+    .then(response => response.text())
+    .then(text => {
+        console.log(text)
+    })
